9b7a966a9c753f63147112c81a22eeef7484c7ba
Md Murad Hossain <mdmuradhossain33@gmail.com>
2021-02-21 23:27:42 +0600

    swagger added

--- src/main/java/io/murad/movie/info/MovieInfoAppApplication.java
+++ src/main/java/io/murad/movie/info/MovieInfoAppApplication.java
@@ -2,8 +2,11 @@ package io.murad.movie.info;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
 
 @SpringBootApplication
+@EnableSwagger2
 public class MovieInfoAppApplication {
 
 	public static void main(String[] args) {
